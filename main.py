395905b3a7488e623056bac9dbea74727a7ef210
AqilaRifti <73378355+AqilaRifti@users.noreply.github.com>
2024-08-25 20:56:20 +0700

    main

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import json
 import fastapi
-from fastapi.middleware import cors
 import random
 from pydantic import BaseModel
+from fastapi.middleware import cors
 
 app = fastapi.FastAPI()
 
@@ -22,23 +22,21 @@ def generate_form_id():
         random_id_list.append(str(random.randint(0, 9)))
     return "".join(random_id_list)
 
-def report_to_db(form_id: str, student_name: str, date: str):
+def report_to_db(form_id: str, student_name: str, date: str, method: str):
     with open("db.json", "r") as file:
         primary_data = json.loads(file.read())
 
     if student_name in primary_data[form_id]["data"]:
         primary_data[form_id]["data"][student_name]["counter"] += 1
         primary_data[form_id]["data"][student_name]["dates"].append(date)
+        primary_data[form_id]["data"][student_name]["methods"].append(method)
         with open("db.json", "w") as file:
             file.write(json.dumps(primary_data))
         return
 
-    primary_data[form_id]["data"][student_name] = {"counter": -3, "dates": list([date])}
+    primary_data[form_id]["data"][student_name] = {"counter": -3, "dates": list([date]), "methods": list([method])}
     with open("db.json", "w") as file:
         file.write(json.dumps(primary_data))
-
-
-
 
 def get_url_from_form_id(form_id: str):
     with open("db.json", "r") as file:
@@ -70,7 +68,7 @@ def create_form(form: FormObject):
 def url_route(form_id: str):
     return {"url": get_url_from_form_id(form_id)}
 
-@app.get("/report/{form_id}/{student_name}/{date}")
-def report_route(form_id: str, student_name: str, date: str):
-    report_to_db(form_id, student_name, date)
+@app.get("/report/{form_id}/{student_name}/{date}/method")
+def report_route(form_id: str, student_name: str, date: str, method: str):
+    report_to_db(form_id, student_name, date, method)
     return {"status": "OK"}
